691074db698c777305da76eaccc57ee2db09ddce
unknown <matheus.santinello@gmail.com>
2022-05-08 22:01:28 -0300

    mobile version of the feedback widget, developed with react native

---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,87 @@
+import { useRef, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import {ChatTeardropDots} from 'phosphor-react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+
+import { Options } from '../Options';
+import { Form } from '../Form';
+import { Success } from '../Success';
+
+import { styles } from './styles';
+import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+
+
+function Widget(){
+
+  const [feedbackType, setFeedbackType]= useState<FeedbackType|null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen(){
+    bottomSheetRef.current?.expand();
+  }
+
+  //Gambiarra abaixo
+  function handleClose(){
+    bottomSheetRef.current?.close();
+  }
+
+  function handleRestartFeedback(){
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
+  function handleFeedbackSent(){
+    setFeedbackSent(true);
+  }
+
+  return (
+      
+      <>
+        <TouchableOpacity 
+        style={styles.button}
+        onPress={handleOpen}
+        onLongPress={handleClose}
+        >
+          <ChatTeardropDots
+          weight="bold"
+          size={24}
+          color={theme.colors.text_on_brand_color} 
+          />
+
+        </TouchableOpacity>
+
+        <BottomSheet
+          ref={bottomSheetRef}
+          snapPoints={[1,280]}
+          backgroundStyle={styles.modal}
+          handleIndicatorStyle={styles.indicator}
+        >
+          {
+            feedbackSent ?
+            <Success onSendAnotherFeedback={handleRestartFeedback}/>
+            :
+            <>
+              {
+                feedbackType?
+                <Form
+                  feedbackType={feedbackType}
+                  onFeedBackCanceled={handleRestartFeedback}
+                  onFeedBackSent={()=>{}}
+
+                />
+                :
+                <Options onFeedbackTypeChanged={setFeedbackType}/>
+              }          
+            </>
+          }
+        </BottomSheet>
+      </>
+  );
+};
+
+export default Widget
